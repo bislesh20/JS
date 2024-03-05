137fb8b577408b9df67b9760b20c61437e17f758
bislesh20 <132096419+bislesh20@users.noreply.github.com>
2024-03-05 05:17:47 +0000

    conatiner config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,19 @@
+const account_Id = 1221                              // constant value cannot be changed 
+let account_Email =  "bisleshsingh1221@gmail.com"   // memory assigned , can be changed
+var account_Password = "9800"
+account_City="Jaipur"
+let account_State;   // if no value assigned to variable js will consider it as undefined
+
+// account_Id = 4556 // not allowed
+
+account_Email="hello@gamil.com"
+account_Password="0000"
+account_City="delhi"
+
+console.log(account_Id);
+
+/*
+Prefer not use var because of the in block space and functional scope
+*/
+
+console.table([account_Id,account_Email,account_Password,account_City,account_State])
