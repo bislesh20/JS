cc85ca4634958104e5cdfa023984d882120f99f5
bislesh20 <132096419+bislesh20@users.noreply.github.com>
2024-03-05 08:37:37 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") we are using node js , not browser -> not ouput here
+
+let name = "Bislesh" 
+let age= 20
+let isLoggedIn= true
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true & false 
+// null => standalone value
+// undefined => no value assigned
+// symbol =>unique
+
+// object 
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isLoggedIn);
+console.log(typeof undefined);
+console.log(typeof null); // null is declared as object
+
