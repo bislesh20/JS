919c55d79cbd0669c94c5404c018fbec15a18baf
bislesh20 <132096419+bislesh20@users.noreply.github.com>
2024-03-05 13:42:55 +0000

    Operations

---
+++ 01_basics/Operations.js
@@ -0,0 +1,38 @@
+let value=5
+let negvalue= -value
+
+//console.log(negvalue)
+
+let str1= "hello"
+let str2= " sam"
+let str3= str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(+true) // converts value of true in number which is 1
+// console.log(true+) // wrong code true+ doest exists
+
+console.log(+"")  // converts "" a string to number (+"")=>0
+
+let counter=100
+
+counter++
+console.log(counter)
+
+++counter
+console.log(counter)
+
+// These both are different operations one is postfix and other is prefix
+// Although th output here is same but they are different 
+
+
+
+
+
+
+
+
