c3b03cc74e765109551907779e037ea4cac347c8
bislesh20 <132096419+bislesh20@users.noreply.github.com>
2024-03-07 14:15:47 +0000

    Strings

---
+++ 01_basics/Strings.js
@@ -0,0 +1,41 @@
+const name="Bislesh"
+let RepoCount= 20
+
+console.log(name+RepoCount)
+
+console.log(`Hello my name is ${name} and repo count is ${RepoCount}`)
+
+const gameName= new String("Bislesh") // another way of declaring a string
+
+console.log(gameName[5])
+console.log(gameName.__proto__)
+
+console.log(gameName.length)
+
+console.log(gameName.toUpperCase())
+
+console.log(gameName.trim())
+
+console.log(gameName.charAt(4))
+
+console.log(gameName.indexOf('s'))
+
+console.log(gameName.substring(3,6))
+
+console.log(gameName.slice(-6,5))
+
+console.log(gameName.slice(-7,5))
+
+const NewString= "   HI_no-space  "
+console.log(NewString.trim())
+
+const url="www.bisleshsingh1221.in"
+console.log(url)
+
+console.log(url.replace('1221','20'))
+ 
+console.log(url.includes('wwww'))
+
+let string ="A quick brown lazy dog jumps over the fox"
+
+console.log(string.split(' '))
